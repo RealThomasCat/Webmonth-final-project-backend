b39d8b9fb1f9806b4e540a8cc6ed639672fe0882
Yashveer Singh <yashveer.rtc@gmail.com>
2022-08-12 18:08:23 +0530

    backend completed

--- controllers/notes.js
+++ controllers/notes.js
@@ -14,3 +14,54 @@ exports.addNote = (req, res) => {
       res.status(400).json({ message: "Database error occured!" });
     });
 };
+
+exports.getAllNotes = (req, res) => {
+  client
+    .query(`SELECT * FROM notes WHERE email = '${req.email}'`)
+    .then((data) => {
+      const noteData = data.rows;
+      const filteredData = noteData.map((note) => {
+        return {
+          noteId: note.noteid,
+          heading: note.heading,
+          content: note.content,
+        };
+      });
+      res.status(200).json({
+        message: "Notes recieved and data filtered successfully!",
+        data: filteredData,
+      });
+    })
+    .catch((err) => {
+      res.status(400).json({ message: "Database error occured!" });
+    });
+};
+
+exports.updateNote = (req, res) => {
+  const noteId = req.params.noteId;
+  const { heading, content } = req.body;
+  client
+    .query(`UPDATE notes SET heading = '${heading}', content = '${content}' WHERE noteid = '${noteId}'`)
+    .then((data) => {
+      res.status(200).json({
+        message: "Note Updated successfully!",
+      });
+    })
+    .catch((err) => {
+      res.status(400).json({ message: "Database error occured!" });
+    });
+};
+
+exports.deleteNote = (req, res) => {
+  const noteId = req.noteId; // extracting noteId using router.param (noteMiddleware)
+  client
+    .query(`DELETE FROM notes WHERE noteid = '${noteId}'`)
+    .then((data) => {
+      res.status(200).json({
+        message: "Note deleted successfully!",
+      });
+    })
+    .catch((err) => {
+      res.status(400).json({ message: "Database error occured!" });
+    });
+};
--- routes/notes.js
+++ routes/notes.js
@@ -1,14 +1,17 @@
 const express = require("express");
-const { addNote } = require("../controllers/notes");
+const { addNote, getAllNotes, updateNote, deleteNote } = require("../controllers/notes");
 const { verifyToken } = require("../middlewares/authMiddleware");
+const { handleNoteIdParam } = require("../middlewares/noteMiddleware");
 const router = express.Router();
 
 module.exports = router;
 
-router.post("/add", verifyToken, addNote);
+router.param("noteId", handleNoteIdParam); // jab bhi kisi ese route par req ayegi jismai noteId hai waha ye chalega
 
+router.post("/add", verifyToken, addNote); //post used for sending data
 
-// localhost:8000/note/add
-// localhost:8000/note/update/:noteId
-// localhost:8000/note/delete/:noteId  
-// localhost:8000/note/getallnotes
+router.get("/getallnotes", verifyToken, getAllNotes); //get used for recieving data
+
+router.put("/update/:noteId", verifyToken, updateNote); //put used for updating data
+
+router.put("/delete/:noteId", verifyToken, deleteNote);
